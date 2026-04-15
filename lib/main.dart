380718a2005bf1e99e0ecc5c40dad6ea3a08073b
Gustavo Pinho <gustavo.r.pinho@aluno.senai.br>
2026-04-15 11:44:56 -0300

    11:44

--- lib/main.dart
+++ lib/main.dart
@@ -10,121 +10,130 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Minha tela")),
-        body: Center(
-          child: Container(
-            padding: EdgeInsets.all(20),
-            width: 350,
-            height: 200,
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: const Color.fromARGB(255, 115, 0, 160),
-                width: 2,
-              ),
-              borderRadius: BorderRadius.circular(15),
-              color: const Color.fromARGB(255, 115, 0, 160),
-            ),
+        appBar: AppBar(
+          title: const Text("Meu Perfil"),
+          centerTitle: true,
+        ),
+        body: SizedBox.expand( 
+          child: SingleChildScrollView(
+      
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center, 
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Text(
-                    "Banco SESI / SENAI",
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-
-                  Icon(Icons.contactless, size: 25, color: Colors.white),
                 
-              ],
-                ),
+           
+Container(
+  padding: const EdgeInsets.all(20),
+  width: 350, 
+  height: 400,
+  decoration: BoxDecoration(
+    borderRadius: BorderRadius.circular(15),
+    color: const Color.fromARGB(255, 146, 0, 0),
+    boxShadow: [
+      BoxShadow(
+        color: Colors.black.withOpacity(0.2),
+        blurRadius: 10,
+        offset: const Offset(0, 5),
+      ),
+    ],
+  ),
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.center, 
+    crossAxisAlignment: CrossAxisAlignment.center, 
+    children: [
+   
+      ClipRRect(
+        borderRadius: BorderRadius.circular(50), 
+        child: Image.asset(
+          'assets/images/guxpinho1.jpeg',
+          height: 100, 
+          width: 100,
+          fit: BoxFit.cover,
+          errorBuilder: (context, error, stackTrace) => 
+              const Icon(Icons.account_circle, size: 100, color: Colors.white),
+        ),
+      ),
+      
+      const SizedBox(height: 20),
 
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-            Icon(
-                Icons.sim_card,
-                size: 35,
-                color: Color.fromARGB(255, 255, 187, 0),
-              ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                
-              Text(
-                "1234 5678 9012 3456",
-                style: TextStyle(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-                  ],
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-              Text(
-                "titular:",
-                style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-                  
-              Text(
-                "Validade",
-                style: TextStyle(
-                  fontSize: 10,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              ],
-                ),
-                Row(
-  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      const Text(
+        "Gustavo Rodrigues",
+        style: TextStyle(
+          fontSize: 22, 
+          color: Colors.white, 
+          fontWeight: FontWeight.bold
+        ),
+      ),
+      
+      const SizedBox(height: 10),
+
+     
+      const Text(
+        "Desenvolvedor Mobile",
+        style: TextStyle(
+          fontSize: 16, 
+          color: Color.fromARGB(146, 255, 255, 255), 
+          fontStyle: FontStyle.italic
+        ),
+      ),
+      const SizedBox(height: 15),
+
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
   children: [
-    Text(
-      "Gustavo",
-      style: TextStyle(
-        fontSize: 20,
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-      ),
-      textAlign: TextAlign.center,
-    ),
-    Text(
-      "12/30",
-      style: TextStyle(
-        fontSize: 20,
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-      ),
-      textAlign: TextAlign.center,
+    const Icon(Icons.email, size: 30, color: Colors.white),
+    const SizedBox(width: 10),
+    const Text(
+      "Gustavoguidolimpinho@gmail.com",
+      style: TextStyle(fontSize: 12, color: Colors.white),
     ),
   ],
+),
+  Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    const Icon(Icons.phone, size: 30, color: Colors.white),
+    const SizedBox(width: 10),
+    const Text(
+      "(11) 99999-9999",
+      style: TextStyle(fontSize: 12, color: Colors.white),
+    ),
+  ],
+),  
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    const Icon(Icons.location_pin, size: 30, color: Colors.white),
+    const SizedBox(width: 10),
+    const Text(
+      "Mococa - SP",
+      style: TextStyle(fontSize: 12, color: Colors.white),
+    ),
+  ],
+),  
+Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    const SizedBox(width: 10),
+    const Icon(Icons.star, size: 30, color: Color.fromARGB(255, 255, 200, 0)),const Icon(Icons.star, size: 30, color: Color.fromARGB(255, 255, 200, 0)),const Icon(Icons.star, size: 30, color: Color.fromARGB(255, 255, 200, 0)),const Icon(Icons.star_half, size: 30, color: Color.fromARGB(255, 255, 200, 0)),
+    const Text(
+      "4,5",
+      style: TextStyle(fontSize: 12, color: Colors.white),
+    ),
+  ],
+),
+    ],
+  ),
 ),
               ],
             ),
           ),
         ),
-      ),
+    ),
     );
   }
 }
---
+++ lib/main_cartões.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp()); //não tirar
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key}); //não tirar
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Carteira Digital"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView( 
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(height: 20),
+
+                
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: 350,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromARGB(255, 146, 0, 0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Banco SESI / SENAI", style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold)),
+                          Icon(Icons.contactless, size: 25, color: Colors.white),
+                        ],
+                      ),
+                      const Icon(Icons.sim_card, size: 35, color: Color.fromARGB(255, 255, 187, 0)),
+                      const Text("1234 5678 9012 3456", style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("titular:", style: TextStyle(fontSize: 10, color: Colors.white)),
+                          Text("Validade", style: TextStyle(fontSize: 10, color: Colors.white)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Gustavo", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                          Text("12/30", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: 350,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: const Color.fromARGB(255, 205, 137, 0),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Tech", style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold)),
+                          Icon(Icons.contactless, size: 25, color: Colors.white),
+                        ],
+                      ),
+                      const Icon(Icons.sim_card, size: 35, color: Color.fromARGB(255, 255, 187, 0)),
+                      const Text("9876 5432 1098 7654", style: TextStyle(fontSize: 25, color: Colors.white, fontWeight: FontWeight.bold)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("titular:", style: TextStyle(fontSize: 10, color: Colors.white)),
+                          Text("Validade", style: TextStyle(fontSize: 10, color: Colors.white)),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Felipe", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                          Text("05/28", style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                
+                Container(
+                  padding: const EdgeInsets.all(20),
+                  width: 350,
+                  height: 200,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.black,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: const [
+                          Text("Black", style: TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.bold)),
+                          Icon(Icons.contactless, size: 25, color: Colors.white),
+                        ],
+                      ),
+                      
+                     
+                      Image.asset(
+                        'assets/images/chip_black.png',
+                        height: 40,
+                        errorBuilder: (context, error, stackTrace) => const Icon(Icons.sim_card, size: 40, color: Colors.grey),
+                      ),
+
+                      const SizedBox(height: 10),
+
+                     
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Gustavo R.G.Pinho",
+                            style: TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold),
+                          ),
+                          Image.asset(
+                            'assets/images/mastercard.png',
+                            height: 35,
+                            errorBuilder: (context, error, stackTrace) => const Icon(Icons.credit_card, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20), 
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
